4a2b36e7acef920846db8b8f8cb1d223ae2b64d6
brooot <1442704671@qq.com>
2019-12-15 18:58:33 +0800

    ad database model

--- forward.py
+++ forward.py
@@ -4,10 +4,10 @@ from socket import *
 from multiprocessing import *
 from Xor import *
 from config import *
-import sys, random, os, time
+import sys, random, os, time, pymysql
 
 
-def recv_from_source():
+def recv_from_source(db, cursor, exp_time, device_id):
     if len(sys.argv) < 3:
         print('''
             argv is error!
@@ -34,15 +34,45 @@ def recv_from_source():
             # 数据存储
             f.write(data + "\n".encode())
 
-    # 将解码出的数据存放到txt文件中
-    print("共收到%d个数据" % recv_num)
+    decode_log = ""
+    for i in recvNum_and_decodeNum:
+        decode_log += "(%d, %d)," % i
+    # 实验次数, 设备id, 实验结果
+    content = (exp_time , device_id, decode_log)
+
+    # 插入语句
+    SQL = "insert into Normal_Transmission (exp_time, device_id, decode_log) values (%d,%s,'%s');" % content
+
+    # 执行插入语句
+    cursor.execute(SQL)
+
+    # 提交
+    db.commit()
+    print("解码过程信息存放在 10.1.18.79 的数据库 中")
+    print("共收到 %d 个码字." % recv_num)
     print("\n数据已存放在 NormalRecv" + str(ADDR) + ".txt 中")
 
     sockfd.close()
 
 
 def main():
-    p1 = Process(target = recv_from_source)
+    # 打开数据库连接
+    db = pymysql.connect(host="10.1.18.79",port=3306,user="root",passwd="chain",db="exp_data",charset='utf8')
+    
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+
+    # 获取上一次实验的实验次数
+    cursor.execute("select exp_time from Normal_Transmission order by id DESC limit 1;")
+    db.commit()
+    data = cursor.fetchone()
+    if data:
+        exp_time = data[0] + 1  # 实验次数加 1
+    else:
+        exp_time = 1
+    device_id = sys.argv[1].split(".")[-1]
+
+    p1 = Process(target = recv_from_source, args=(db, cursor, exp_time, device_id))
     p1.start()
     p1.join()
 
